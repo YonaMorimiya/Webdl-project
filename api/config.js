d2f6b1b1273d19f6a1b921ffa021edf61813a68f
Lizetta Morimiya <156690876+YonaMorimiya@users.noreply.github.com>
2026-05-15 15:04:57 +0800

    Add files via upload

---
+++ api/config.js
@@ -0,0 +1,51 @@
+/*
+• Jasa pembuatan website 
+• Menerima perbaikan script atau fitur bot
+• Menerima pembuatan fitur bot
+• Menerima semua kebutuhan bot
+• Menerima dia dengan segala kekurangannya;)
+ℹ️ Information
+
+• Bisa bayar di awal atau akhir
+• Pembayaran melalu QRIS Only
+• Testimoni Banyak
+
+        ••JANGAN HAPUS INI••
+SCRIPT BY © VYNAA VALERIE 
+•• recode kasih credits 
+•• contacts: (6282389924037)
+•• instagram: @vynaa_valerie 
+•• (github.com/VynaaValerie) 
+*/
+module.exports = {
+  API_KEY: 'sk-45b3672346c7',
+  API_BASE: 'https://api.vtech.biz.id/api/download',
+  ENDPOINTS: {
+    tiktok: '/tiktok',
+    tiktokslide: '/tiktokslide',
+    instagram: '/igdowloader',
+    youtube: '/ytdlv2',
+    facebook: '/fbdown',
+    pinterest: '/pinterest',
+    threads: '/threads',
+    capcut: '/capcut',
+    cocofun: '/cocofun',
+    snackvideo: '/snackvideo',
+    spotify: '/spotify',
+    gdrive: '/gdrive'
+  },
+  TEST_URLS: {
+    tiktok: 'https://vt.tiktok.com/ZSeJ7P56G',
+    tiktokslide: 'https://vt.tiktok.com/ZSHGno9d2L4w1-thgJL/',
+    instagram: 'https://www.instagram.com/reel/DREcZAakbis/?igsh=YXpvbDRmMHQxcGw5',
+    youtube: 'https://www.youtube.com/watch?v=HyhLsy6b0XI',
+    facebook: 'https://www.facebook.com/watch/?v=1393572814172251',
+    pinterest: 'https://pin.it/4CVodSq',
+    threads: 'https://www.threads.net/t/Cujx6ryoYx6/',
+    capcut: 'https://www.capcut.com/template-detail/7299286607478181121?template_id=7299286607478181121',
+    cocofun: 'https://www.icocofun.com/share/post/379250110809',
+    snackvideo: 'https://s.snackvideo.com/p/j9jKr9dR',
+    spotify: 'https://open.spotify.com/track/3zakx7RAwdkUQlOoQ7SJRt',
+    gdrive: 'https://drive.google.com/file/d/1thDYWcS5p5FFhzTpTev7RUv0VFnNQyZ4/view'
+  }
+};
